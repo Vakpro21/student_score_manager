a82d2751cb28802d97986355c1b9c4e6223a4bed
Khoa Vo123 <kvo@academic.rrc.ca>
2025-08-12 22:06:36 -0500

    script.js: updated data storing function.

--- script.js
+++ script.js
@@ -3,50 +3,56 @@
  * Description: Main script to utilize front-end features
  * Author: Khoa Vo
  * Created on: 2025-06-27
-*/
-function subjectManager () {
-    const userTypedSubject = document.getElementById("subject").value.trim();
-    const userBasicSubject = document.getElementById("subject-choice").value;
+ */
+function subjectManager() {
+  const userTypedSubject = document.getElementById("subject").value.trim();
+  const userBasicSubject = document.getElementById("subject-choice").value;
 
-    // If user typed a new subject
-    if (userTypedSubject !== "") {
-        return userTypedSubject;
-    };
+  // If user typed a new subject
+  if (userTypedSubject !== "") {
+    return userTypedSubject;
+  }
 
-    // If user selected a subject from dropdown (not "default")
-    if (userBasicSubject !== "default") {
-        return userBasicSubject;
-    };
+  // If user selected a subject from dropdown (not "default")
+  if (userBasicSubject !== "default") {
+    return userBasicSubject;
+  }
 
-    // If both are empty or invalid
-    return false;
-};
+  // If both are empty or invalid
+  return false;
+}
 
-function validate () {
-    let input = [];
-    // const userName
-    // const userId
-    // const userScore
-};
+function validate() {
+  const subject = subjectManager();
+  let input = { userName: "", userId: "", userCourse: "", userScore: "" };
+  input.userName = document.getElementById("name").value.trim();
+  input.userScore = document.getElementById("score").value.trim();
+  input.userId = document.getElementById("student-id").value.trim();
+  if (subject !== false) {
+    input.userCourse = subject;
+  }
 
-function removeData () {
+  return input;
+}
 
-};
+function removeData() {}
 
-function avgCalulator () {
+function avgCalculator() {}
 
-};
+function errorDisplay() {}
 
-function errorDisplay () {
+function clearScreen() {
+  const form = document.getElementById("student-form");
+  form.reset();
+}
 
-};
+function run() {
+  document.getElementById("add-btn").addEventListener("click", (event) => {
+    event.preventDefault();
+    const test = validate();
+    console.log(test);
+    clearScreen();
+  });
+}
 
-function run () {
-    document.getElementById("add-btn").addEventListener("submit", (event) => {
-        event.preventDefault();
-        const test = subjectManager ();
-        console.log(test);
-    });
-};
-
-run ();
+run();
